6b808f3f337eb6616fbad76add7ad1179ec8b22f
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-28 14:31:43 +0530

    ADDED:Method/Regex for matching valid Email

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,8 +2,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistration
 {
-    //PATTERN FOR NAME
+    //PATTERN FOR NAME,EMAIL
     private static final String NAME_PATTERN = "^[A-Z][A-Za-z0-9]{2,}$";
+    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]+([.+-]?[0-9a-zA-z])*[@][a-zA-Z0-9]+([.][a-zA-Z]{2,}){1,2}$";
 
     //PATTERN TO VALIDATE FIRST NAME
     public boolean validateFirstName(String firstName)
@@ -18,5 +19,12 @@ public class UserRegistration
     {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lastName).matches();
+    }
+
+    //PATTERN TO VALIDATE EMAIL ID
+    public boolean validateEmail(String email)
+    {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
