431025ca9d51d156641335f7bf7d0c5c752bef2d
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-28 15:32:54 +0530

    ADDED:Test methods for checking minimum 8 chars for password

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,28 +1,24 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UserRegistrationTest
-{
+public class UserRegistrationTest {
     //TEST CASES FOR FIRST NAME
     @Test
-    public void givenValidFirstNameReturnsTrue()
-    {
+    public void givenValidFirstNameReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateFirstName("Ranbir");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void givenFirstNameStartWithSmallerCaseReturnsFalse()
-    {
+    public void givenFirstNameStartWithSmallerCaseReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateFirstName("ranbir");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void givenFirstnameHasTwoCharacterReturnsFalse()
-    {
+    public void givenFirstnameHasTwoCharacterReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateFirstName("An");
         Assert.assertFalse(result);
@@ -31,24 +27,21 @@ public class UserRegistrationTest
 
     //TEST CASES FOR LAST NAME
     @Test
-    public void givenValidLastNameReturnsTrue()
-    {
+    public void givenValidLastNameReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("Singh");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void givenLastNameStartWithSmallerCaseReturnsFalse()
-    {
+    public void givenLastNameStartWithSmallerCaseReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("singh");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void givenLastNameHasTwoCharacterReturnFalse()
-    {
+    public void givenLastNameHasTwoCharacterReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("Si");
         Assert.assertFalse(result);
@@ -57,8 +50,7 @@ public class UserRegistrationTest
 
     //TEST CASES FOR LAST NAME
     @Test
-    public void givenMobileNumberInPreDefinedFormatReturnsTrue()
-    {
+    public void givenMobileNumberInPreDefinedFormatReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("92 9876543210");
         Assert.assertTrue(result);
@@ -66,18 +58,33 @@ public class UserRegistrationTest
     }
 
     @Test
-    public void givenMobileNumberWithNineDigitReturnsFalse()
-    {
+    public void givenMobileNumberWithNineDigitReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("91 123456789");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void givenMobileNumberWithoutSpaceReturnsFalse()
-    {
+    public void givenMobileNumberWithoutSpaceReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("919874563210");
+        Assert.assertFalse(result);
+    }
+
+    //TEST CASES FOR PASSWORD
+    @Test
+    public void givenPasswordWithMinimumEightCharacterReturnTrue()
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validPassword("qwer5478");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPasswordWithLessThanEightCharacterReturnFalse()
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validPassword("qwe123");
         Assert.assertFalse(result);
     }
 }
