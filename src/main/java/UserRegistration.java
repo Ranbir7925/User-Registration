8860493a45db3e48acfdcab5747ac22052d43ad1
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-28 14:56:26 +0530

    ADDED:Method/Regex for validating mobile number

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,11 +2,12 @@ import java.util.regex.Pattern;
 
 public class UserRegistration
 {
-    //PATTERN FOR NAME,EMAIL
+    //PATTERN FOR NAME,EMAIL,MOBILE NUMBER.
     private static final String NAME_PATTERN = "^[A-Z][A-Za-z0-9]{2,}$";
     private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]+([.+-]?[0-9a-zA-z])*[@][a-zA-Z0-9]+([.][a-zA-Z]{2,}){1,2}$";
+    private static final String MOBILE_NO_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
 
-    //PATTERN TO VALIDATE FIRST NAME
+    //METHOD TO VALIDATE FIRST NAME
     public boolean validateFirstName(String firstName)
     {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -14,17 +15,24 @@ public class UserRegistration
 
     }
 
-    //PATTERN TO VALIDATE LAST NAME
+    //METHOD TO VALIDATE LAST NAME
     public boolean validateLastName(String lastName)
     {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lastName).matches();
     }
 
-    //PATTERN TO VALIDATE EMAIL ID
+    //METHOD TO VALIDATE EMAIL ID
     public boolean validateEmail(String email)
     {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(email).matches();
+    }
+
+    //METHOD TO VALIDATE MOBILE NUMBER
+    public boolean validateMobileNumber(String mobileNumber)
+    {
+        Pattern pattern = Pattern.compile(MOBILE_NO_PATTERN);
+        return pattern.matcher(mobileNumber).matches();
     }
 }
