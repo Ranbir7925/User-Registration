fadf5ba73bbc3773a46cc0e72a79026d366ca5ea
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-29 14:31:01 +0530

    REFACTORED:removed objects which are repeated

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,25 +1,31 @@
+import com.bridgelabz.userregistration.UserRegistration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+    UserRegistration userRegistration = null;
+
+    @Before
+    public void setUp() throws Exception {
+         userRegistration = new UserRegistration();
+    }
+
     //TEST CASES FOR FIRST NAME
     @Test
-    public void givenValidFirstNameReturnsTrue() {
-        UserRegistration userRegistration = new UserRegistration();
+    public void givenValid_FirstName_ReturnsTrue() {
         boolean result = userRegistration.validateFirstName("Ranbir");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenFirstNameStartWithSmallerCaseReturnsFalse() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateFirstName("ranbir");
         Assert.assertFalse(result);
     }
 
     @Test
     public void givenFirstnameHasTwoCharacterReturnsFalse() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateFirstName("An");
         Assert.assertFalse(result);
     }
@@ -28,30 +34,26 @@ public class UserRegistrationTest {
     //TEST CASES FOR LAST NAME
     @Test
     public void givenValidLastNameReturnsTrue() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("Singh");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenLastNameStartWithSmallerCaseReturnsFalse() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("singh");
         Assert.assertFalse(result);
     }
 
     @Test
     public void givenLastNameHasTwoCharacterReturnFalse() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("Si");
         Assert.assertFalse(result);
     }
 
 
-    //TEST CASES FOR LAST NAME
+    //TEST CASES FOR MOBILE NUMBER
     @Test
     public void givenMobileNumberInPreDefinedFormatReturnsTrue() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("92 9876543210");
         Assert.assertTrue(result);
 
@@ -59,14 +61,12 @@ public class UserRegistrationTest {
 
     @Test
     public void givenMobileNumberWithNineDigitReturnsFalse() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("91 123456789");
         Assert.assertFalse(result);
     }
 
     @Test
     public void givenMobileNumberWithoutSpaceReturnsFalse() {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("919874563210");
         Assert.assertFalse(result);
     }
@@ -75,7 +75,6 @@ public class UserRegistrationTest {
     @Test
     public void givenPasswordWithMinimumEightCharacterReturnTrue()
     {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validPassword("qwer5478");
         Assert.assertTrue(result);
     }
@@ -83,7 +82,6 @@ public class UserRegistrationTest {
     @Test
     public void givenPasswordWithLessThanEightCharacterReturnFalse()
     {
-        UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validPassword("qwe123");
         Assert.assertFalse(result);
     }
